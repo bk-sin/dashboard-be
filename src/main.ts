d909bcf89cb39065053659d9a396538b6d6cd19b
Emiliano Alegre <emiliano.alegre@vortex-it.com>
2025-07-29 20:07:56 -0300

    feat: add passport authentication and validation, enable CORS

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { PrismaClientExceptionFilter } from './prisma-client-exception/prisma-client-exception.filter';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -20,6 +21,18 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
 
   app.useGlobalFilters(new PrismaClientExceptionFilter(httpAdapter));
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidUnknownValues: false,
+    }),
+  );
+
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
 
   SwaggerModule.setup('api-docs', app, document);
 
